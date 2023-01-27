639828ace6592e917f837c5f01f8931d1fd612ee
Yang Jiefeng <yangjiefeng@gmail.com>
2023-01-27 22:21:59 +0800

    update project `dotnet-t4`

--- dotnet-t4/VisualStudioTextTemplateHost.cs
+++ dotnet-t4/VisualStudioTextTemplateHost.cs
@@ -34,7 +34,7 @@ using EnvDTE80;
 using Microsoft.VisualStudio.TextTemplating;
 using Microsoft.VisualStudio.TextTemplating.VSHost;
 using T4Toolbox;
-//using AIT.Tools.VisualStudioTextTransform.Properties;
+using Mono.TextTemplating.Properties;
 
 namespace Mono.TextTemplating
 {
@@ -86,42 +86,39 @@ namespace Mono.TextTemplating
 		protected override ITextTemplatingSession CreateSession () => new ToolTemplateSession (this);
 
 		public object GetService(Type serviceType) 
-		{
-			// Source.TraceEvent(TraceEventType.Verbose, 0, Resources.VisualStudioTextTemplateHost_GetService_Service_request_of_type___0_, serviceType);
-			// if (serviceType == typeof (DTE) || serviceType == typeof (DTE2))
-			// {
-			//     Source.TraceEvent(TraceEventType.Verbose, 0, Resources.VisualStudioTextTemplateHost_GetService_Returning_DTE_instance_);
-			//     return _dte;
-			// }
-			// // ///////////////////////////////////
-			// if (serviceType == typeof(T4Toolbox.ITransformationContextProvider))//TextTemplateHostSettings.Default.GetType("T4Toolbox.ITransformationContextProvider"))
-			// {
-			//     if (_transformationContextProvider == null)
-			//     {
-			//         var cp = new TransformationContextProvider(this);
-			//         cp.ProjectFullPath = this.ProjectFullPath;
-			//         _transformationContextProvider = cp;
-			//     }
-			//     return _transformationContextProvider;
-			//     // -----------------------------------
-			//     //var serviceImplType = typeof(T4Toolbox.VisualStudio.ScriptFileGenerator).Assembly
-			//     //    .GetType("T4Toolbox.VisualStudio.TransformationContextProvider");
-			//     //ConstructorInfo ctr = serviceImplType.GetConstructor(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public, null, new Type[] { typeof(IServiceProvider) }, new ParameterModifier[0]);
-			//     //var _transformationContextProvider = (T4Toolbox.ITransformationContextProvider)ctr.Invoke(new object[] { this });
-			//     //return _transformationContextProvider;
-			//     // -------------------------------------
-			//     //Type t4packType = typeof(T4Toolbox.VisualStudio.ScriptFileGenerator).Assembly.GetType("T4Toolbox.VisualStudio.T4ToolboxPackage");
-			//     //var container = (System.ComponentModel.Design.IServiceContainer)Activator.CreateInstance(t4packType);
-			//     //MethodInfo initMethod = t4packType.GetMethod("Initialize", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-			//     //initMethod.Invoke(container, new object[0]);
-			//     //var service = container.GetService(serviceType);
-			//     //return serviceType;
-			// }
-			// if (serviceType == typeof(STextTemplating))
-			// {
-			//     return this;
-			// }
-			// ///////////////////////////////////
+		{           
+			Source.TraceEvent(TraceEventType.Verbose, 0, Resources.VisualStudioTextTemplateHost_GetService_Service_request_of_type___0_, serviceType);
+			if (serviceType == typeof (DTE) || serviceType == typeof (DTE2))
+			{
+			    Source.TraceEvent(TraceEventType.Verbose, 0, Resources.VisualStudioTextTemplateHost_GetService_Returning_DTE_instance_);
+			    return _dte;
+			}
+            /*
+            * needed services:
+            * ITextTemplatingEngineHost,
+            * ITextTemplatingComponents,
+            * ITransformationContextProvider
+            */
+            if (serviceType == typeof(ITextTemplatingEngineHost))
+			{
+			    return (ITextTemplatingEngineHost)this;
+			} 
+            if (serviceType == typeof(ITextTemplatingComponents))
+			{
+			    return (ITextTemplatingComponents)this;
+			}		   
+            if (serviceType == typeof(ITransformationContextProvider))//TextTemplateHostSettings.Default.GetType("T4Toolbox.ITransformationContextProvider"))
+			{
+			    if (_transformationContextProvider == null)
+			    {
+                    //todo fill _transformationContextProvider
+			        // var cp = new TransformationContextProvider(this);
+			        // cp.ProjectFullPath = this.ProjectFullPath;
+			        // _transformationContextProvider = cp;
+			    }
+			    return _transformationContextProvider;
+			}
+			
 			return null;
 		}
 
